244fea32b2f7d09e5003e70a769681e97d3bad1d
Lucas Molina Miranda <lmmiranda@stefanini.com>
2024-02-01 13:26:16 -0300

    feat: Adding tag component to library

---
+++ src/lib/components/tag/Tag.stories.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+    import Tag from './Tag.svelte';
+</script>
+  <Meta
+    title="Tag"
+    component={Tag}
+    argTypes={{
+    }}
+  />
+  
+  <Template let:args>
+    <Tag
+      {...args}
+    >
+    Tag Test
+    </Tag>
+  </Template>
+  
+  <Story
+    name="Default"
+    args={{
+    color: 'red',
+    size: 'lg',
+    padding: '4px 16px'
+    }}
+  />
+  
