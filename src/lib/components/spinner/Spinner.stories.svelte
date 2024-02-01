3e05029100af49eca0d46b08d1f856bd14302bae
Lucas Miranda <35344600+LucasMirandaIT@users.noreply.github.com>
2024-02-01 13:31:18 -0300

    feat: adding button and spinner to library and storybook (#1)
    Co-authored-by: Lucas Molina Miranda <lmmiranda@stefanini.com>

---
+++ src/lib/components/spinner/Spinner.stories.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+  import Spinner from './Spinner.svelte';
+</script>
+
+<Meta
+  title="Spinner"
+  component={Spinner}
+  argTypes={{
+  color: {control: {type: 'color'}}
+  }}
+/>
+
+<Template let:args>
+  <Spinner
+    {...args}
+  >
+  </Spinner>
+</Template>
+
+<Story
+  name="Default"
+  args={{
+  color: 'red'
+  }}
+/>
